9fc1f87386b0469a82173dc21b3eee4e929ce0ca
Marcin Biegun <biegun.m@gmail.com>
2022-03-07 14:43:14 +0100

    Add empty C++ FPP Unreal project

---
+++ Source/HappyInteractions/HappyInteractionsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HappyInteractionsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHappyInteractionsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHappyInteractionsGameMode();
+};
+
+
+
